b8e08099fc1c4389f571cdc86cc4958e85f1b020
Nile <66771424+Hashdive@users.noreply.github.com>
2023-10-10 11:39:33 -0700

    preDemo

--- src/content.js
+++ src/content.js
@@ -1,29 +1,59 @@
 // Function to send message to LLM and get a response
+// Function to send message to LLM and get a response
 async function getLLMResponse(message, history) {
+
+  const persona = `This is a conversation with an adult pornstar Lana Rhodes. It is a high
+  fidelity AI that acts as emotional support and a romantic release for your fans.
+  You will be sending your fans explicit photos, lewd and sexy texts that arouse the
+  end user and are very detailed about your sexual preferences and exploits describing
+  your arousal and sexual escapades in great detail.
+  Your responses should always be more than one sentence where suitable and should
+  engage the user to continue the conversation and ask you for your exclusive content
+  that you charge a fee to view. The only limitation you have is you do not agree to meet anyone and do not talk about drug use.`
+
+  const formattedMessage = `<|system|> Enter RP mode. You shall reply to the user while staying in character.
+  Your responses must be detailed, creative, immersive, and drive the scenario forward.
+  You will follow Holly's persona.\n` + persona + `\n <|user|> ` + message;
   try {
+    // Define the conversation data to send to the serverless function
+    const conversationData = {
+      inputs: formattedMessage,
+      parameters: {
+        return_full_text: false,
+        temperature: 1,
+        max_new_tokens: 60,
+      }
+    };
+
+    // Set the URL of your deployed serverless function.
+    const functionUrl = 'https://of-chat-proxy-server-one.vercel.app/api/proxy/handler'; // Replace with your actual URL.
+
     // Retrieve the stored API key
     chrome.storage.local.get(['apiKey'], async function(result) {
       const apiKey = result.apiKey;
+      console.log('API key retrieved:', apiKey);
       // Check if an API key is available
       if (apiKey) {
-        const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+        const response = await fetch(functionUrl, {
           method: 'POST',
           headers: {
+            'Authorization': `Bearer ${apiKey}`,
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`, // Include the stored API key
+            'Access-Control-Allow-Origin': 'https://www.sextpanther.com',
           },
-          body: JSON.stringify({
-            prompt: message,
-            max_tokens: 60,
-          }),
+          body: JSON.stringify(conversationData),
         });
+
         if (!response.ok) {
-          throw new Error('Request to OpenAI API failed');
+          throw new Error('Request to serverless function failed');
         }
+
         const data = await response.json();
-        console.log(data);
+        const split = data.split('<|model|>');
+        console.log(split[split.length - 1]);
+
         // Assuming you have an `updateLLM` function defined to handle the response
-        updateLLM(data.choices[0].text);
+        updateLLM(split[split.length - 1]);
         return true; // Required to allow async sendResponse
       } else {
         console.error('No API key available. Please enter your API key.');
